6c1cd2d545549bf18290d5af980c84022920f5ba
Laura Andrade <lauraandradecamp@gmail.com>
2023-12-05 21:48:12 -0300

    Exercising while structure

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿string opcao;
+
+while(true) 
+{
+  Console.WriteLine("Digite a sua opção:");
+  Console.WriteLine("1 - Cadastrar cliente");
+  Console.WriteLine("2 - Buscar cliente");
+  Console.WriteLine("3 - Apagar cliente");
+  Console.WriteLine("4 - Encerrar");
+
+  opcao = Console.ReadLine();
+
+  switch(opcao) {
+    case "1":
+      Console.WriteLine("Cadastro do cliente");
+      break;
+    case "2":
+      Console.WriteLine("Busca do cliente");
+      break;
+    case "3":
+      Console.WriteLine("Apagar do cliente");
+      break;
+    case "4":
+      Console.WriteLine("Encerrando...");
+      Environment.Exit(0);
+      break;
+    
+    default:
+      Console.WriteLine("Opção inválida");
+      break;
+  }
+}
